0da23159503a27499bf2e3e8dd37a26173b6dead
SHAIK AFRID <88675474+afridanon@users.noreply.github.com>
2021-11-21 00:07:45 -0500

    Update Encapsulation.java

--- Java/OOPS/Encapsulation.java
+++ Java/OOPS/Encapsulation.java
@@ -1,24 +1,26 @@
+//securing the data(sensitive data )
+
 package javaproject;
 // Encapsulation
 class bank{
-	private String username = "afrid";
-	private int password = 143;
-	public void SetUsername(String username) {
+	private String username = "afrid";     // sensitive username 
+	private int password = 143;          //sensitive password
+	public void SetUsername(String username) {              //method to change username
 		this.username = username;
 	}
-	public String GetUsername() {
+	public String GetUsername() {                     //method to get username for validation
 		return username;
 	}
-	public int GetPassword() {
+	public int GetPassword() {                      //       method to get password
 		return password ;
 	}
-	public void SetPassword(int password) {
+	public void SetPassword(int password) {               //method to change password
 		this.password = password;
 	}
 }
-
-public class Encapsulation{
-	public static void main(String [] args) {
-		bank a = new bank();
+ 
+public class Encapsulation{             //main class
+	public static void main(String [] args) {                    //main method of main class
+		bank a = new bank();                  //object
 	}
 }
